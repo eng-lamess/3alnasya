e6ede4de6dd8cb1df96e9dc5cc513ce3f237918b
eng-lamess <eng-lamess@hotmail.com>
2016-08-09 11:09:05 +0300

    ttest

---
+++ database/migrations/2016_08_07_120017_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->string('video');
+            $table->string('author');
+            $table->text('photo');
+            $table->dateTime('date');
+            $table->integer('rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('blogs');
+    }
+}
---
+++ resources/views/blogs/create.blade.php
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+
+<div class="row">
+<div class="col-md-8 col-md-offset-2">
+<h2>create new post</h2>
+<hr>
